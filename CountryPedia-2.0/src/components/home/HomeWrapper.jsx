8e53eb722f113145b9a011c10ac85546b27516b7
Julia <jsgoransson@gmail.com>
2024-05-02 19:04:42 +0200

    some restructuring for better readability

--- CountryPedia-2.0/src/components/home/HomeWrapper.jsx
+++ CountryPedia-2.0/src/components/home/HomeWrapper.jsx
@@ -2,11 +2,12 @@ import styled from "@emotion/styled";
 import CountriesWrapper from "./CountriesWrapper";
 
 const MainContent = styled.div`
-  height: 100svh;
+  height: 100vh;
   width: calc(100vw - 4rem);
   display: flex;
   flex-direction: column;
   padding: 1rem 2rem;
+  transition: all 0.3s linear;
 `;
 
 function HomeWrapper() {
