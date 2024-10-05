89de013b9758d8cebab84ea007c70298b79ca7b1
2021.sneha.utekar@ves.ac.in <2021.sneha.utekar@ves.ac.in>
2024-10-05 15:44:47 +0530

    counter for right guesses

--- CountryPedia-2.0/src/components/games/FlagQuiz/Result.jsx
+++ CountryPedia-2.0/src/components/games/FlagQuiz/Result.jsx
@@ -1,3 +1,24 @@
+// import React from "react";
+// import styled from "@emotion/styled";
+
+// const ResultBox = styled.div`
+//   width: 100%;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   padding: 2rem;
+//   background-color: white;
+// `;
+
+// function Result({ resultMessage }) {
+//   return (
+//     <ResultBox>
+//       <h2>{resultMessage}</h2>
+//     </ResultBox>
+//   );
+// }
+
+// export default Result;
 import React from "react";
 import styled from "@emotion/styled";
 
